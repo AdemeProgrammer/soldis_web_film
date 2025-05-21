2d3bca419489ebafdc1e44a32c89ba7040131f43
Ademe <A.AYAD@lprs.fr>
2025-05-21 11:47:05 +0200

    [feat] Ajout trait_inscription, trait_connexion
    Ajout des pages de traitement pour l'inscription et la connexion

---
+++ src/traitement/trait_connexion.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../bdd/Bdd.php';
+require_once '../../vue/Connexion.php';
+require_once '../modele/Utilisateurs.php';
+require_once '../Repository/UtilisateursRepository.php';
+
+if (!isset($_POST["email"]) || !isset($_POST["mot_de_passe"]) || empty($_POST["email"]) || empty($_POST["mot_de_passe"])) {
+    echo "C'est pas bien, vous avez laissé une case vide";
+    header("Location: ../../vue/Connexion.php");
+    exit();
+} else {
+    $utilisateurs = new Utilisateurs([
+        'email' => $_POST["email"],
+        'motDePasse' => $_POST["mot_de_passe"]
+    ]);
+
+    $utilisateursRepository = new UtilisateursRepository();
+    $resultat = $utilisateursRepository->connexionUtilisateurs($utilisateurs);
+
+    if ($resultat) {
+        session_start();
+
+        $_SESSION['id_utilisateur'] = $utilisateurs->getIdUtilisateur();
+        $_SESSION['nom'] = $utilisateurs->getNom();
+        $_SESSION['prenom'] = $utilisateurs->getPrenom();
+        $_SESSION['email'] = $utilisateurs->getEmail();
+        $_SESSION['motDePasse'] = $utilisateurs->getMotDePasse();
+        $_SESSION['role'] = $utilisateurs->getRole();
+
+        header("Location: ../../vue/Accueil.php");
+        exit();
+    } else {
+        session_destroy();
+        header("Location: ../../vue/Connexion.php");
+        exit();
+    }
+}
+?>
---
+++ src/traitement/trait_inscription.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../bdd/Bdd.php';
+require_once '../../vue/Inscription.php';
+require_once '../modele/Utilisateurs.php';
+require_once '../Repository/UtilisateursRepository.php';
+
+if (empty($_POST["nom"]) || empty($_POST["prenom"]) || empty($_POST["email"]) || empty($_POST["mot_de_passe"])) {
+    echo "C'est pas bien ...";
+    header("Location: ../../vue/InscriptionEM.php");
+    exit();
+} else {
+    $utilisateurs = new Utilisateurs([
+        'nom' => $_POST['nom'],
+        'prenom' => $_POST["prenom"],
+        'email' => $_POST['email'],
+        'motDePasse' => password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT),
+        'role' => "Client",
+    ]);
+
+    $UtilisateursRepository = new UtilisateursRepository();
+    $resultat = $UtilisateursRepository->ajoutUtilisateurs($utilisateurs);
+
+    if ($resultat) {
+        header("Location: ../../vue/Connexion.php");
+    } else {
+        header("Location: ../../vue/Inscription.php");
+    }
+}
+?>
